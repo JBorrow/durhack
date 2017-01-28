eea4b64f600dc01e807b89f7571d2743952e9e67
Josh Borrow <josh.borrow@gmail.com>
2017-01-28 23:02:06 +0000

    fuckin added mondays, tuesday and wednesdays to the mad stuff ( for real thought javascript arrays are now used to populate data for streets )

--- web/source/javascripts/all.js
+++ web/source/javascripts/all.js
@@ -1,7 +1,17 @@
 // This is where it all goes :)
 
 window.onload = function () {
+	days = ['monday', 'tuesday', 'weds'];
+	weather_pics = ['sun-o', 'bolt', 'cloud'];
+	weather_temps = [22, 23, 23];
+	ffall = [2123, 1231, 2343];
+	ffallpc = [22, 22, 24];
+
 	pages = ['silva', 'x'];
+
+        silva_data = pretty_data(days, weather_pics, weather_temps, ffall, ffallpc);
+	document.getElementById('silva_data').innerHTML = silva_data;
+	document.getElementById('x_data').innerHTML = silva_data;
 
 	current_page = 0;
 
@@ -33,3 +43,12 @@ function left() {
   }
 }
 
+function pretty_data(days, weather_types, weather_temps, ffall, ffall_pc) {
+  // Prints the data prettily into your id :)
+  data_op = ""
+  for (var i=0; i < days.length; i++) {
+    this_data = "<div class='vbox my2'><div class='flex justify-between'><div class='day'>" + days[i] + "</div><div class='weather'><i class='fa fa-" + weather_types[i] + " fa-lg'></i> " + weather_temps[i] + " C</div><div class='footfall'>" + ffall[i] + " (" + ffall_pc[i] + ")</div></div></div>";
+    data_op += this_data;
+  }
+  return data_op;
+}
