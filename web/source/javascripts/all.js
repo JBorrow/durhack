985862748ca99273795bac08312551f8bba8d72c
Josh Borrow <josh.borrow@gmail.com>
2017-01-28 22:37:11 +0000

    added js carousel for data

--- web/source/javascripts/all.js
+++ web/source/javascripts/all.js
@@ -1,1 +1,35 @@
 // This is where it all goes :)
+
+window.onload = function () {
+	pages = ['silva', 'x'];
+
+	current_page = 0;
+
+	display_pages(current_page);
+}
+
+function display_pages(page) {
+  for (var i=0; i < pages.length; i++) {
+    console.log(pages[i])
+    if (i == page) {
+      document.getElementById(pages[i]).style.display = "block";
+    } else {
+      document.getElementById(pages[i]).style.display = "none";
+    }
+  }
+}
+
+function right() {
+  if (current_page + 1 < pages.length) {
+    current_page += 1;
+    display_pages(current_page);
+  }
+}
+
+function left() {
+  if (current_page - 1 >= 0) {
+    current_page -= 1;
+    display_pages(current_page);
+  }
+}
+
